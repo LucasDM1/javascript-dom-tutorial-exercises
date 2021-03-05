e57772ca7c55d96352f86f1bd11c577b63f04aa7
LucasDM1 <lucas19651@hotmail.com>
2021-03-05 03:17:11 +0000

    2left

--- .breathecode/resets/05-Create-DOM-Element-First/index.js
+++ .breathecode/resets/05-Create-DOM-Element-First/index.js
@@ -4,7 +4,3 @@ document.querySelector("#myDiv").appendChild(elem);
 
 //Your code here
 // Don'change the code above
-var elem1 = document.createElement("P");
-elem1.innerHTML = "Hello World";
-document.querySelector("#myDiv").appendChild(elem1);
-elem1.style.background = "yellow";
--- exercises/13-Add-Options-to-the-Select/index.js
+++ exercises/13-Add-Options-to-the-Select/index.js
@@ -2,5 +2,10 @@ window.onload = function() {
 	console.log("The website was loaded...");
 	let countries = ["USA", "France", "Italy", "Brazil", "Colombia", "Belize", "Venezuela"];
 
-  // your code here
+	// your code here
+	countries.forEach(element => {
+		let elem = document.createElement("OPTION");
+		elem.innerHTML = element;
+		document.querySelector("#mySelect").appendChild(elem);
+	});
 };
