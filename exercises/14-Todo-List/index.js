1fb30b9f8e07c461a16cfdebdf577a06ba29536b
LucasDM1 <lucas19651@hotmail.com>
2021-03-06 03:05:54 +0000

    Finished

--- exercises/14-Todo-List/index.js
+++ exercises/14-Todo-List/index.js
@@ -1,1 +1,32 @@
 // Your code here
+
+let input = document.querySelector("#addToDo");
+input.addEventListener("change", () => {
+	//your code here
+
+	let newElement = document.createElement("LI");
+	newElement.innerHTML = `<span><i class="fa fa-trash"></i></span> ${input.value}`;
+	document.querySelector("ul").appendChild(newElement);
+	Deletetask();
+});
+function Deletetask() {
+	let list = document.querySelectorAll("ul li");
+	list.forEach((item, index) => {
+		item.addEventListener("click", event => {
+			let removeEle = event.target.parentNode.parentNode;
+			removeEle.parentNode.removeChild(removeEle);
+		});
+	});
+}
+Deletetask();
+
+// let listItems = document.querySelectorAll('.ranges li');
+
+// listItems.forEach((item, index) => {
+//   item.addEventListener('click', (event) => {
+//      alert(`${event.currentTarget.innerHTML} item was click`);
+//   });
+//   if (item.innerHTML.indexOf('Last 30 days') != -1) {
+//       item.click();
+//   }
+// });
